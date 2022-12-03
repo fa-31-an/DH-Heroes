219616ae0967f9730768b08dafb855f59e1462fe
Fabián Ariel <fsosaolea@gmail.com>
2022-12-03 12:16:44 -0300

    Enlaces enlazados

--- app.js
+++ app.js
@@ -2,9 +2,35 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Holi')
-})
+});
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/babbage.html'))
+});
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/bernes-lee.html'))
+});
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/clarke.html'))
+});
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
+});
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+});
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+});
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/turing.html'))
+});
 
 app.listen(3030, () => {
     console.log('The server is ON at the port 3030')
