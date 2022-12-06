305de6ef1387bf5e606552a581b8291acea52d5d
Fabián Ariel <fsosaolea@gmail.com>
2022-12-06 19:02:58 -0300

    Arregladas B Lee, Clarke. Añadidos enlaces a inicio/páginas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Holi')
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
@@ -14,7 +14,7 @@ app.get('/babbage', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/babbage.html'))
 });
 app.get('/berners-lee', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/bernes-lee.html'))
+    res.sendFile(path.join(__dirname, '/views/berners-lee.html'))
 });
 app.get('/clarke', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/clarke.html'))
